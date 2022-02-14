40ab61fbea64abcf673e1c180068a69b6a4f3a91
osaha <adhocosam@gmail.com>
2022-02-14 20:55:15 +0900

    first commit

---
+++ Tool2/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool2
+{
+    public class Log
+    {
+        public string Date { get; set; }
+        public string Guid { get; set; }
+        public string User { get; set; }
+
+        public Log(string date, string guid, string user)
+        {
+            Date = date;
+            Guid = guid;
+            User = user;
+        }
+    }
+}
